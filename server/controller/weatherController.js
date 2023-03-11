11479ec92b708938a25064b033193a9e49e1bc5b
Phil <phil.luc89@gmail.comm>
2023-03-11 12:59:02 -0500

    weather done

--- server/controller/weatherController.js
+++ server/controller/weatherController.js
@@ -6,18 +6,22 @@ const API_KEY = 'dead5c9b084e896a899ac3479c1d786c'
 
 weatherController.getData = async (req,res,next) => {
   console.log('Inside get weather middleware')
-  const { zip } = req.body;
-  const WEATHER_URL_ZIP = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${API_KEY}&units=imperial`;
+  let { zip, metric } = req.body;
+  if (!metric) {
+    metric = 'imperial';
+  }
+  const WEATHER_URL_ZIP = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${API_KEY}&units=${metric}`;
   const weatherReq = await fetch(WEATHER_URL_ZIP);
   const apiResponse = await weatherReq.json();
   console.log('apiResponsee', apiResponse)
   const responseObj = {
-    type: apiResponse.weather[0].main,
+    type: apiResponse.weather[0].main.toLowerCase(),
     temp: apiResponse.main.temp,
+    metric,
     zip,
     city: apiResponse.name,
     iconUrl: `https://openweathermap.org/img/wn/${apiResponse.weather[0].icon}@2x.png`,
-  }
+  };
 //   console.log(apiResponse)
   res.locals.response = responseObj;
   return next();
