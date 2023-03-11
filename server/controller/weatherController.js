76b925012e6ee47dce79ee13c709559bf57e41cb
Phil <phil.luc89@gmail.comm>
2023-03-11 12:41:33 -0500

    weather done

--- server/controller/weatherController.js
+++ server/controller/weatherController.js
@@ -5,10 +5,12 @@ const API_KEY = 'dead5c9b084e896a899ac3479c1d786c'
 
 
 weatherController.getData = async (req,res,next) => {
-  const {zip} = req.body;
+  console.log('Inside get weather middleware')
+  const { zip } = req.body;
   const WEATHER_URL_ZIP = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${API_KEY}&units=imperial`;
   const weatherReq = await fetch(WEATHER_URL_ZIP);
   const apiResponse = await weatherReq.json();
+  console.log('apiResponsee', apiResponse)
   const responseObj = {
     type: apiResponse.weather[0].main,
     temp: apiResponse.main.temp,
@@ -16,7 +18,7 @@ weatherController.getData = async (req,res,next) => {
     city: apiResponse.name,
     iconUrl: `https://openweathermap.org/img/wn/${apiResponse.weather[0].icon}@2x.png`,
   }
-  console.log(apiResponse)
+//   console.log(apiResponse)
   res.locals.response = responseObj;
   return next();
 };
--- server/server.js
+++ server/server.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const app = express();
 const path = require('path');
+const PORT = 3000;
 
 const weatherRouter = require('./routes/weather')
 
 app.use(express.json());
 
+
 app.use('/weather', weatherRouter);
 
+app.use('*', (req, res) => res.sendStatus(404));
 
 
 app.use((err, req, res) => {
@@ -22,9 +25,9 @@ app.use((err, req, res) => {
   return res.status(errorObj.status).json(errorObj.message);
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('Server is running on port 3000');
+  console.log(`Server is running on port ${PORT}`);
 });
 
 
